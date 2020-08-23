d7d641a38103bc0f011471f65e1177af6e177c5f
GANGASAGAR HL <55350412+Gangasagarhl@users.noreply.github.com>
2020-08-23 12:42:15 +0530

    Add files via upload

---
+++ packages/centroidtracker.py
@@ -0,0 +1,128 @@
+# import the necessary packages
+from scipy.spatial import distance as dist
+from collections import OrderedDict
+import numpy as np
+
+class CentroidTracker:
+	def __init__(self, maxDisappeared=50, maxDistance=50):
+		# initialize the next unique object ID along with two ordered
+		# dictionaries used to keep track of mapping a given object
+		# ID to its centroid and number of consecutive frames it has
+		# been marked as "disappeared", respectively
+		self.nextObjectID = 0
+		self.objects = OrderedDict()
+		self.disappeared = OrderedDict()
+
+		
+		self.maxDisappeared = maxDisappeared
+
+		
+		self.maxDistance = maxDistance
+
+	def register(self, centroid):
+		# when registering an object we use the next available object
+		# ID to store the centroid
+		self.objects[self.nextObjectID] = centroid
+		self.disappeared[self.nextObjectID] = 0
+		self.nextObjectID += 1
+
+	def deregister(self, objectID):
+		# to deregister an object ID we delete the object ID from
+		# both of our respective dictionaries
+		del self.objects[objectID]
+		del self.disappeared[objectID]
+
+	def update(self, rects):
+		# check to see if the list of input bounding box rectangles
+		# is empty
+		if len(rects) == 0:
+			# loop over any existing tracked objects and mark them
+			# as disappeared
+			for objectID in list(self.disappeared.keys()):
+				self.disappeared[objectID] += 1
+
+				# if we have reached a maximum number of consecutive
+				# frames where a given object has been marked as
+				# missing, deregister it
+				if self.disappeared[objectID] > self.maxDisappeared:
+					self.deregister(objectID)
+
+			# return early as there are no centroids or tracking info
+			# to update
+			return self.objects
+
+		# initialize an array of input centroids for the current frame
+		inputCentroids = np.zeros((len(rects), 2), dtype="int")
+
+		# loop over the bounding box rectangles
+		for (i, (startX, startY, endX, endY)) in enumerate(rects):
+			# use the bounding box coordinates to derive the centroid
+			cX = int((startX + endX) / 2.0)
+			cY = int((startY + endY) / 2.0)
+			inputCentroids[i] = (cX, cY)
+
+		# if we are currently not tracking any objects take the input
+		# centroids and register each of them
+		if len(self.objects) == 0:
+			for i in range(0, len(inputCentroids)):
+				self.register(inputCentroids[i])
+
+		
+		else:
+			# grab the set of object IDs and corresponding centroids
+			objectIDs = list(self.objects.keys())
+			objectCentroids = list(self.objects.values())
+			D = dist.cdist(np.array(objectCentroids), inputCentroids)
+			rows = D.min(axis=1).argsort()
+			cols = D.argmin(axis=1)[rows]
+			usedRows = set()
+			usedCols = set()
+
+			# loop over the combination of the (row, column) index
+			# tuples
+			for (row, col) in zip(rows, cols):
+				
+				if row in usedRows or col in usedCols:
+					continue
+				
+				if D[row, col] > self.maxDistance:
+					continue
+				# otherwise, grab the object ID for the current row,
+				# set its new centroid, and reset the disappeared
+				# counter
+				objectID = objectIDs[row]
+				self.objects[objectID] = inputCentroids[col]
+				self.disappeared[objectID] = 0
+
+				# indicate that we have examined each of the row and
+				# column indexes, respectively
+				usedRows.add(row)
+				usedCols.add(col)
+                
+                
+
+			
+			unusedRows = set(range(0, D.shape[0])).difference(usedRows)
+			unusedCols = set(range(0, D.shape[1])).difference(usedCols)
+
+			
+			if D.shape[0] >= D.shape[1]:
+				# loop over the unused row indexes
+				for row in unusedRows:
+					
+					objectID = objectIDs[row]
+					self.disappeared[objectID] += 1
+
+					
+					if self.disappeared[objectID] > self.maxDisappeared:
+						self.deregister(objectID)
+
+			# otherwise, if the number of input centroids is greater
+			# than the number of existing object centroids we need to
+			# register each new input centroid as a trackable object
+			else:
+				for col in unusedCols:
+					self.register(inputCentroids[col])
+
+		# return the set of trackable objects
+		return self.objects
---
+++ people_counter.py
@@ -0,0 +1,271 @@
+# USAGE
+# To read and write back out to video:
+# python people_counter.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt \
+#	--model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --input videos/example_01.mp4 \
+#	--output output/output_01.avi
+#
+# To read from webcam and write back out to disk:
+# python people_counter.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt \
+#	--model mobilenet_ssd/MobileNetSSD_deploy.caffemodel \
+#	--output output/webcam_output.avi
+
+# import the necessary packages
+from packages.centroidtracker import CentroidTracker
+from packages.trackableobject import TrackableObject
+from imutils.video import VideoStream
+from imutils.video import FPS
+import numpy as np
+import argparse
+import imutils
+import time
+import dlib
+import cv2
+
+
+# initialize the list of class labels MobileNet SSD was trained to
+# detect
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor"]
+
+# load our serialized model from disk
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe(r"modelfiles/MobileNetSSD_deploy.prototxt", r"modelfiles/MobileNetSSD_deploy.caffemodel")
+
+# if a video path was not supplied, grab a reference to the webcam
+if not (r"videos/example_01.mp4", False):
+	print("[INFO] starting video stream...")
+	vs = VideoStream(src=0).start()
+	time.sleep(2.0)
+
+# otherwise, grab a reference to the video file
+else:
+	print("[INFO] opening video file...")
+	vs = cv2.VideoCapture("videos/example_01.mp4")
+
+# initialize the video writer (we'll instantiate later if need be)
+writer = None
+
+
+W = None
+H = None
+
+
+ct = CentroidTracker(maxDisappeared=40, maxDistance=50)
+trackers = []
+trackableObjects = {}
+
+
+totalFrames = 0
+totalDown = 0
+totalUp = 0
+skip_frames = 30
+
+# start the frames per second throughput estimator
+fps = FPS().start()
+
+# loop over frames from the video stream
+while True:
+	# grab the next frame and handle if we are reading from either
+	# VideoCapture or VideoStream
+	sucess ,frame = vs.read()
+
+
+	# if we are viewing a video and we did not grab a frame then we
+	# have reached the end of the video
+	if "videos/example_01.mp4" is not None and frame is None:
+		break
+
+	
+	frame = imutils.resize(frame, width=500)
+	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+	# if the frame dimensions are empty, set them
+	if W is None or H is None:
+		(H, W) = frame.shape[:2]
+
+	# if we are supposed to be writing a video to disk, initialize
+	# the writer
+	if "output" is not None and writer is None:
+		fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+		writer = cv2.VideoWriter(r"output/output_02.avi", fourcc, 30,
+			(W, H), True)
+
+	
+	status = "Waiting"
+	rects = []
+
+	
+	if totalFrames % skip_frames == 0:
+		# set the status and initialize our new set of object trackers
+		status = "Detecting"
+		trackers = []
+
+		# convert the frame to a blob and pass the blob through the
+		# network and obtain the detections
+		blob = cv2.dnn.blobFromImage(frame, 0.007843, (W, H), 127.5)
+		net.setInput(blob)
+		detections = net.forward()
+
+		# loop over the detections
+		for i in np.arange(0, detections.shape[2]):
+			# extract the confidence (i.e., probability) associated
+			# with the prediction
+			confidence = detections[0, 0, i, 2]
+
+			# filter out weak detections by requiring a minimum
+			# confidence
+			if confidence > 0.4:
+				# extract the index of the class label from the
+				# detections list
+				idx = int(detections[0, 0, i, 1])
+
+				# if the class label is not a person, ignore it
+				if CLASSES[idx] != "person":
+					continue
+
+				# compute the (x, y)-coordinates of the bounding box
+				# for the object
+				box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
+				(startX, startY, endX, endY) = box.astype("int")
+
+				# construct a dlib rectangle object from the bounding
+				# box coordinates and then start the dlib correlation
+				# tracker
+				tracker = dlib.correlation_tracker()
+				rect = dlib.rectangle(startX, startY, endX, endY)
+				tracker.start_track(rgb, rect)
+
+				# add the tracker to our list of trackers so we can
+				# utilize it during skip frames
+				trackers.append(tracker)
+
+	# otherwise, we should utilize our object *trackers* rather than
+	# object *detectors* to obtain a higher frame processing throughput
+	else:
+		# loop over the trackers
+		for tracker in trackers:
+			# set the status of our system to be 'tracking' rather
+			# than 'waiting' or 'detecting'
+			status = "Tracking"
+
+			# update the tracker and grab the updated position
+			tracker.update(rgb)
+			pos = tracker.get_position()
+
+			# unpack the position object
+			startX = int(pos.left())
+			startY = int(pos.top())
+			endX = int(pos.right())
+			endY = int(pos.bottom())
+
+			# add the bounding box coordinates to the rectangles list
+			rects.append((startX, startY, endX, endY))
+
+	# draw a horizontal line in the center of the frame -- once an
+	# object crosses this line we will determine whether they were
+	# moving 'up' or 'down'
+	cv2.line(frame, (0, H // 2), (W, H // 2), (0, 255, 255), 2)
+
+	# use the centroid tracker to associate the (1) old object
+	# centroids with (2) the newly computed object centroids
+	objects = ct.update(rects)
+
+	# loop over the tracked objects
+	for (objectID, centroid) in objects.items():
+		# check to see if a trackable object exists for the current
+		# object ID
+		to = trackableObjects.get(objectID, None)
+
+		# if there is no existing trackable object, create one
+		if to is None:
+			to = TrackableObject(objectID, centroid)
+
+		# otherwise, there is a trackable object so we can utilize it
+		# to determine direction
+		else:
+			# the difference between the y-coordinate of the *current*
+			# centroid and the mean of *previous* centroids will tell
+			# us in which direction the object is moving (negative for
+			# 'up' and positive for 'down')
+			y = [c[1] for c in to.centroids]
+			direction = centroid[1] - np.mean(y)
+			to.centroids.append(centroid)
+
+			# check to see if the object has been counted or not
+			if not to.counted:
+				# if the direction is negative (indicating the object
+				# is moving up) AND the centroid is above the center
+				# line, count the object
+				if direction < 0 and centroid[1] < H // 2:
+					totalUp += 1
+					to.counted = True
+
+				# if the direction is positive (indicating the object
+				# is moving down) AND the centroid is below the
+				# center line, count the object
+				elif direction > 0 and centroid[1] > H // 2:
+					totalDown += 1
+					to.counted = True
+
+		# store the trackable object in our dictionary
+		trackableObjects[objectID] = to
+
+		# draw both the ID of the object and the centroid of the
+		# object on the output frame
+		text = "ID {}".format(objectID)
+		cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+		cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
+
+	# construct a tuple of information we will be displaying on the
+	# frame
+	info = [
+		("Up", totalUp),
+		("Down", totalDown),
+		("Status", status),
+	]
+
+	# loop over the info tuples and draw them on our frame
+	for (i, (k, v)) in enumerate(info):
+		text = "{}: {}".format(k, v)
+		cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+
+	# check to see if we should write the frame to disk
+	if writer is not None:
+		writer.write(frame)
+
+	# show the output frame
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+
+	# if the `q` key was pressed, break from the loop
+	if key == ord("q"):
+		break
+
+	# increment the total number of frames processed thus far and
+	# then update the FPS counter
+	totalFrames += 1
+	fps.update()
+
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# check to see if we need to release the video writer pointer
+if writer is not None:
+	writer.release()
+
+# if we are not using a video file, stop the camera video stream
+if not (r"videos/example_02.mp4", False):
+	vs.stop()
+
+# otherwise, release the video file pointer
+else:
+	vs.release()
+
+# close any open windows
+cv2.destroyAllWindows()
